ddbbb83418ff58eb6540ecd50ad024f247448772
Dustin Kahn <dustin@dustinkahn.com>
2016-10-15 12:20:49 -0400

    sign out link added to nav bar along with passing tests

--- spec/features/user_signs_in_spec.rb
+++ spec/features/user_signs_in_spec.rb
@@ -50,5 +50,16 @@ feature 'user signs in' do
       visit new_user_session_path
       expect(page).to have_content('You are already signed in')
     end
+
+    scenario 'an authenticated user can sign out' do
+      visit new_user_session_path
+      fill_in 'Email', with: user.email
+      fill_in 'Password', with: user.password
+      click_button 'Sign In'
+      click_link 'Sign Out'
+
+      expect(page).to have_content('Sign In')
+      expect(page).to_not have_content('Sign Out')
+    end
   end
 end
