399d01829ec5c6e215b91d62f29344a08e44a97d
Dustin Kahn <dustin@dustinkahn.com>
2016-10-18 10:54:51 -0400

    test for presence of monologue new form

---
+++ spec/features/user_views_monologue_new_page_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+feature 'user visits monologue new page' do
+  context 'as a user I want to vist the monologue new page' do
+    let!(:user) { FactoryGirl.create(:user) }
+
+    scenario 'so that I can view a form for uploading a monologue' do
+      visit root_path
+      click_link 'Sign In'
+      fill_in 'Email', with: user.email
+      fill_in 'Password', with: user.password
+      click_button 'Sign In'
+      click_link 'Add A New Monologue'
+
+      expect(page).to have_css("form")
+      expect(page).to have_content("Upload A Monologue")
+      expect(page).to_not have_content("#{user.first_name}'s Profile")
+    end
+  end
