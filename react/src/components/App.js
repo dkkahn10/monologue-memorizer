d321590df29b57f654c32fe1bba46319433d7918
Dustin Kahn <dustin@dustinkahn.com>
2016-10-20 17:08:05 -0400

    button added to replace react show page text with a text box

--- react/src/components/App.js
+++ react/src/components/App.js
@@ -5,13 +5,23 @@ class App extends Component {
     super(props);
     this.state = {
       memory: "",
+      mem_page: null,
       monologue: null,
-      upload: null
+      upload: null,
     }
+    this.memoryTesterClick = this.memoryTesterClick.bind(this)
   }
 
   update(screen_text) {
     this.setState({memory: screen_text.target.value})
+  }
+
+  memoryTesterClick() {
+    if(this.state.mem_page === null) {
+      this.setState({mem_page: ""});
+    } else {
+      this.setState({mem_page: null})
+    }
   }
 
   getMonologueId() {
@@ -34,15 +44,31 @@ class App extends Component {
 
   render(){
     if (this.state.monologue !== null) {
-      return(
-        <div>
+      if (this.state.mem_page !== null) {
+        return(
+          <div>
+            <button onClick={this.memoryTesterClick}>
+              {`Back To Your Monologue`}
+            </button><br/>
+            <input type="text"
+              onChange={this.update.bind(this)} />
+            <p>{this.state.memory}</p>
+          </div>
+        )
+      } else {
+        return(
+          <div>
           <h1>{this.state.monologue.play_title}</h1>
           <h2>{this.state.monologue.character}</h2>
           <h2>{`Page Number: ${this.state.monologue.page_number}`}</h2>
           <h3>{this.state.monologue.genre}</h3>
           <p>{this.state.upload}</p>
-        </div>
-      );
+          <button onClick={this.memoryTesterClick}>
+            {`Test Your Memory!`}
+          </button>
+          </div>
+        );
+      }
     } else {
       return(
         <div> </div>
