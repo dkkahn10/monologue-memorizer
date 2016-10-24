f4836a7a6abd010e0f49435df47d1e29bb260eaa
Dustin Kahn <dustin@dustinkahn.com>
2016-10-24 11:31:46 -0400

    tests change to accomadate new style

--- spec/features/user_visits_profile_page_spec.rb
+++ spec/features/user_visits_profile_page_spec.rb
@@ -6,7 +6,6 @@ feature 'user visits profile page' do
 
     scenario 'so that I know that I am signed in' do
       visit root_path
-      click_link 'Sign In'
       fill_in 'Email', with: user.email
       fill_in 'Password', with: user.password
       click_button 'Sign In'
