a189e13d0298af42013b3c7d2210597fc7216cdc
Dustin Kahn <dustin@dustinkahn.com>
2016-10-28 13:41:43 -0400

    coveralls filter

---
+++ spec/factories/monologue.rb
@@ -0,0 +1,10 @@
+FactoryGirl.define do
+  factory :user do
+    sequence(:email) { |n| "link#{n}@hyrulecastle.com" }
+    first_name 'Link'
+    last_name 'Courage'
+    password 'zelda1212'
+    password_confirmation 'zelda1212'
+    user
+  end
+end
